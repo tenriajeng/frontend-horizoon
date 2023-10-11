4fc0aad0759db36da1db4b509200025f1d845844
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-10-11 16:52:09 +0800

    update categories

--- app/page.jsx
+++ app/page.jsx
@@ -2,35 +2,12 @@ import getCourses from '@/api/getCourses';
 import CourseCategories from '@/components/course-categories';
 import Image from 'next/image';
 
-const courseCategories = [
-    'Web Development',
-    'Data Science',
-    'Design',
-    'Marketing',
-    'Business',
-    'Mobile App Development',
-    'Machine Learning',
-    'Photography',
-    'Finance',
-    'Artificial Intelligence',
-    'Programming Languages',
-    'Health and Fitness',
-    'Cooking',
-    'Music',
-    'Mathematics',
-    'History',
-    'Science',
-    'Psychology',
-    'Language Learning',
-    'Self-Improvement',
-];
-
 export default async function Home() {
     const courses = await getCourses();
 
     return (
         <>
-            <CourseCategories categories={courseCategories} />
+            <CourseCategories />
             <div className="course-list xs:mx-2 md:mx-12 xs:mt-2 md:mt-6">
                 <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xs:gap-2 md:gap-4">
                     {courses.data.map((course) => (

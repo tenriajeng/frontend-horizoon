9df01bd8610d10c95a2621113addab925e65f76d
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-10-11 17:03:48 +0800

    add course page

---
+++ app/courses/[slug].jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CourseDeatil() {
+    return <div>CourseDeatil</div>;
+}
+
+export default CourseDeatil;
--- components/course-categories.jsx
+++ components/course-categories.jsx
@@ -13,16 +13,6 @@ const CourseCategories = () => {
     ];
 
     return (
-        // <div className="overflow-x-auto xs:mt-2 md:mt-6 xs:mx-2 md:mx-12 flex xs:space-x-2 md:space-x-4">
-        //     {courseCategories.map((category, index) => (
-        //         <Button
-        //             key={index}
-        //             className="xs:px-10 xs:py-8 md:px-14 md:py-10 text-xs text-black bg-gray-100 hover:bg-gray-200 dark:hover:bg-gray-800 dark:bg-gray-900 dark:text-white transition duration-300 ease-in-out transform hover:scale-105"
-        //         >
-        //             {category}
-        //         </Button>
-        //     ))}
-        // </div>
         <div className="grid grid-cols-1 xs:grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-8 xs:gap-2 md:gap-4 xs:mt-2 md:mt-6 xs:mx-2 md:mx-12">
             {courseCategories.map((category, index) => (
                 <Button

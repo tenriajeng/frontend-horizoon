21ae089a06e980b4f2e08853e4d28fd461cdf1ed
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-05 19:24:16 +0800

    Update checkout.jsx

--- components/checkout.jsx
+++ components/checkout.jsx
@@ -7,13 +7,13 @@ export default function Checkout() {
     const total = useSelector((state) => state.totalPrice.total);
     return (
         <div className="w-full justify-center space-y-2 rounded-lg p-2">
-            <h4 className="font-bold">Total : </h4>
-            <h4 className="text-2xl font-bold">
+            <h2 className="font-bold">Total : </h2>
+            <h2 className="text-2xl font-bold">
                 IDR{' '}
                 {` ${Number(total).toLocaleString('id-ID', {
                     maximumFractionDigits: 3,
                 })}`}
-            </h4>
+            </h2>
             <Button variant="outline" className="w-full font-bold">
                 Checkout
             </Button>

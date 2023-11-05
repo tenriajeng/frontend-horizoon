38904752ec145376af6ff8082ed1cb36ec4a06b2
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-05 21:59:14 +0800

    Update add-to-cart-button.jsx

--- components/add-to-cart-button.jsx
+++ components/add-to-cart-button.jsx
@@ -5,11 +5,21 @@ import { DialogLogin } from './dialog-login';
 import { FaPlus } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 
-const AddToCartButton = () => {
+const AddToCartButton = ({ slug }) => {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
+    function handleAddToCart(event) {
+        event.stopPropagation();
+        console.log('slug ', slug);
+    }
+
     return isAuthenticated ? (
-        <Button variant="outline" size="sm" className="xs:hidden md:flex">
+        <Button
+            variant="outline"
+            size="sm"
+            className="xs:hidden md:flex"
+            onClick={handleAddToCart}
+        >
             <FaPlus className="mr-1 h-3 w-3" /> Cart
         </Button>
     ) : (

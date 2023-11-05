9bfccea1a3b18406fe4bd38ddc5f22cf64373710
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-05 20:29:13 +0800

    Update carts.jsx

--- components/carts.jsx
+++ components/carts.jsx
@@ -2,10 +2,35 @@
 
 import { IoCartOutline } from 'react-icons/io5';
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { getAuthToken } from '@/lib/authUtils';
+import getCarts from '@/api/cart/getCarts';
+import { setCartCount } from '@/redux/features/cartCountSlice';
 
 export default function Carts() {
     const count = useSelector((state) => state.cartCount.count);
+    const dispatch = useDispatch();
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCartData = async () => {
+            const authToken = getAuthToken();
+
+            if (authToken) {
+                try {
+                    const cartData = await getCarts(authToken);
+                    dispatch(setCartCount(cartData.data.length));
+                } catch (error) {
+                    console.error('Error while fetching carts:', error);
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+        };
+        fetchCartData();
+    }, [dispatch]);
 
     return (
         <div className="relative">
@@ -16,7 +41,7 @@ export default function Carts() {
                 <span className="sr-only">View Shopping Cart</span>
                 <IoCartOutline className="h-6 w-6" />
             </Link>
-            <span className="absolute right-0 top-0 -mr-1 -mt-2 flex h-4 items-center justify-center rounded-full border bg-white px-[2px] text-xs font-bold text-slate-900">
+            <span className="absolute right-0 top-0 -mr-1 -mt-2 flex h-4 items-center justify-center rounded-full border bg-white px-1 text-xs font-bold text-slate-900">
                 {count}
             </span>
         </div>

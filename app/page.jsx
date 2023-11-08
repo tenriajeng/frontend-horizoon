0853f0647e30ff5720454d3e81556d7b0273ea57
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-08 21:54:27 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -9,6 +9,8 @@ import {
 } from '@/components/ui/hover-card';
 import { CalendarIcon } from '@radix-ui/react-icons';
 import Image from 'next/image';
+import Loading from './loading';
+import { Suspense } from 'react';
 
 export default async function Home() {
     // await new Promise((resolve) => setTimeout(resolve, 10000));
@@ -16,47 +18,49 @@ export default async function Home() {
     const courses = await getCourses();
 
     return (
-        <div className="md:container">
-            <CourseCategories />
-            <div className="mt-4 xs:mx-0 xs:p-0 md:mx-2 ">
-                <div className="grid grid-cols-1  xs:mx-2 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
-                    {courses.data.map((course) => (
-                        <HoverCard key={course.id}>
-                            <HoverCardTrigger asChild>
-                                <CoursesCard course={course} />
-                            </HoverCardTrigger>
-                            <HoverCardContent className="w-96" side="left">
-                                <div className="flex justify-between space-x-4">
-                                    <div className="space-y-1">
-                                        <Image
-                                            priority
-                                            loading="eager"
-                                            width={400}
-                                            height={400}
-                                            src={course.thumbnail}
-                                            alt={course.title}
-                                            className="aspect-video rounded-lg border object-cover "
-                                        />
+        <Suspense fallback={<Loading />}>
+            <div className="md:container">
+                <CourseCategories />
+                <div className="mt-4 xs:mx-0 xs:p-0 md:mx-2 ">
+                    <div className="grid grid-cols-1  xs:mx-2 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
+                        {courses.data.map((course) => (
+                            <HoverCard key={course.id}>
+                                <HoverCardTrigger asChild>
+                                    <CoursesCard course={course} />
+                                </HoverCardTrigger>
+                                <HoverCardContent className="w-96" side="left">
+                                    <div className="flex justify-between space-x-4">
+                                        <div className="space-y-1">
+                                            <Image
+                                                priority
+                                                loading="eager"
+                                                width={400}
+                                                height={400}
+                                                src={course.thumbnail}
+                                                alt={course.title}
+                                                className="aspect-video rounded-lg border object-cover "
+                                            />
 
-                                        <h2 className="text-base font-medium">
-                                            {course.title}
-                                        </h2>
-                                        <p className="text-justify text-sm dark:text-gray-400">
-                                            {course.meta_description}
-                                        </p>
-                                        <div className="flex items-center pt-2">
-                                            <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{' '}
-                                            <span className="text-xs text-muted-foreground">
-                                                Joined December 2021
-                                            </span>
+                                            <h2 className="text-base font-medium">
+                                                {course.title}
+                                            </h2>
+                                            <p className="text-justify text-sm dark:text-gray-400">
+                                                {course.meta_description}
+                                            </p>
+                                            <div className="flex items-center pt-2">
+                                                <CalendarIcon className="mr-2 h-4 w-4 opacity-70" />{' '}
+                                                <span className="text-xs text-muted-foreground">
+                                                    Joined December 2021
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </HoverCardContent>
-                        </HoverCard>
-                    ))}
+                                </HoverCardContent>
+                            </HoverCard>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </Suspense>
     );
 }

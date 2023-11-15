9e7fc9c94eb496cdfa0384b789cdaba42dcb0a02
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-15 22:24:01 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import CourseCategories from '@/components/course-categories';
 import Courses from '@/components/courses';
-import Pagination from '@/components/pagination';
 
 export default async function Home() {
     return (
@@ -9,7 +8,6 @@ export default async function Home() {
             <div className="mt-4 xs:mx-0 xs:p-0 md:mx-2 ">
                 <Courses />
             </div>
-            <Pagination />
         </div>
     );
 }

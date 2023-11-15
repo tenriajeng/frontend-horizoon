6e5e33e23614f53bbb0abf1d7058b1ef41b3d736
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-15 22:24:07 +0800

    Update courses.jsx

--- components/courses.jsx
+++ components/courses.jsx
@@ -2,18 +2,22 @@ import getCourses from '@/api/getCourses';
 import CoursesCard from './courses-card';
 import LoadingCoursesCard from './loading/courses-card';
 import { Suspense } from 'react';
+import Pagination from './pagination';
 
 const Courses = async () => {
-    const courses = await getCourses();
+    const courses = await getCourses(67);
 
     return (
-        <div className="mb-10 grid grid-cols-1 xs:mx-2 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
-            <Suspense fallback={<LoadingCoursesCard />}>
-                {courses.data.map((item, index) => (
-                    <CoursesCard key={index} course={item} />
-                ))}
-            </Suspense>
-        </div>
+        <>
+            <div className="mb-10 grid grid-cols-1 xs:mx-2 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
+                <Suspense fallback={<LoadingCoursesCard />}>
+                    {courses.data.map((item, index) => (
+                        <CoursesCard key={index} course={item} />
+                    ))}
+                </Suspense>
+            </div>
+            <Pagination pagination={courses.pagination} />
+        </>
     );
 };
 

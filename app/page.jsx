116035f357c498be3050327926b2f601e25bc832
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-16 23:16:50 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,20 @@
 import Carousel from '@/components/carousel';
 import CourseCategories from '@/components/course-categories';
 import Courses from '@/components/courses';
+import LoadingCourseCategories from '@/components/loading/course-categories';
 import LoadingCoursesCard from '@/components/loading/courses-card';
 import { Suspense } from 'react';
 
 export default async function Home() {
     return (
         <>
-            <Carousel />
+            <Suspense fallback={<h1>Loading ...</h1>}>
+                <Carousel />
+            </Suspense>
             <div className="md:container">
-                <CourseCategories />
+                <Suspense fallback={<LoadingCourseCategories />}>
+                    <CourseCategories />
+                </Suspense>
                 <div className="mt-4 xs:mx-0 xs:p-0 md:mx-2 ">
                     <div className="grid grid-cols-1 xs:mx-2 xs:mb-5 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:mb-10 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
                         <Suspense fallback={<LoadingCoursesCard />}>
--- components/course-categories.jsx
+++ components/course-categories.jsx
@@ -1,6 +1,3 @@
-import { Suspense } from 'react';
-import LoadingCourseCategories from './loading/course-categories';
-
 const CourseCategories = () => {
     const courseCategories = [
         'Artificial Intelligence',
@@ -14,18 +11,16 @@ const CourseCategories = () => {
     ];
 
     return (
-        <Suspense fallback={<LoadingCourseCategories />}>
-            <div className="grid grid-cols-1 xs:mx-2 xs:mt-2 xs:grid-cols-4 xs:gap-2 sm:grid-cols-4 md:mt-4 md:grid-cols-4 md:gap-5 lg:grid-cols-8">
-                {courseCategories.map((category, index) => (
-                    <div
-                        key={index}
-                        className="flex transform cursor-pointer items-center justify-center rounded-lg border bg-white px-2 text-xs text-black transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-50  dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900 xs:py-4 md:py-7 lg:w-full"
-                    >
-                        <span className="text-center">{category}</span>
-                    </div>
-                ))}
-            </div>
-        </Suspense>
+        <div className="grid grid-cols-1 xs:mx-2 xs:mt-2 xs:grid-cols-4 xs:gap-2 sm:grid-cols-4 md:mt-4 md:grid-cols-4 md:gap-5 lg:grid-cols-8">
+            {courseCategories.map((category, index) => (
+                <div
+                    key={index}
+                    className="flex transform cursor-pointer items-center justify-center rounded-lg border bg-white px-2 text-xs text-black transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-50  dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900 xs:py-4 md:py-7 lg:w-full"
+                >
+                    <span className="text-center">{category}</span>
+                </div>
+            ))}
+        </div>
     );
 };
 

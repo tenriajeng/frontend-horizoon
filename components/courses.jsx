46a51c2bc55ce2b3c86decb0fc342f6be11ab619
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-16 15:04:21 +0800

    Update courses.jsx

--- components/courses.jsx
+++ components/courses.jsx
@@ -33,10 +33,13 @@ const Courses = () => {
     return (
         <>
             <div className="grid grid-cols-1 xs:mx-2 xs:mb-5 xs:mt-2 xs:grid-cols-2 xs:gap-2 sm:grid-cols-2 md:mx-0 md:mb-10 md:grid-cols-3 md:gap-5 lg:grid-cols-4">
-                {!isLoading &&
+                {!isLoading ? (
                     courses.data.map((item, index) => (
                         <CoursesCard key={index} course={item} />
-                    ))}
+                    ))
+                ) : (
+                    <LoadingCoursesCard />
+                )}
             </div>
             {!isLoading && <Pagination pagination={courses.pagination} />}
         </>

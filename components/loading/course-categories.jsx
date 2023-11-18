b908f2cab8db735be9d4fae28f2cb8c6a3c2c030
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-19 00:01:02 +0800

    Update course-categories.jsx

--- components/loading/course-categories.jsx
+++ components/loading/course-categories.jsx
@@ -3,14 +3,14 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function LoadingCourseCategories() {
     return (
         <>
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
-            <Skeleton className="w-full xs:py-6 md:py-9" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
+            <Skeleton className="h-16 w-full" />
         </>
     );
 }

3ff7819f4d97c323e486b99acc0715510262a553
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-19 00:32:08 +0800

    Update home-courses.jsx

--- components/loading/home-courses.jsx
+++ components/loading/home-courses.jsx
@@ -7,10 +7,10 @@ export default async function LoadingHomeCourses() {
             {Array.from({ length: 4 }, (_, index) => (
                 <div key={index} className="mb-4">
                     <div className="mb-2 flex items-end justify-between">
-                        <Skeleton className="h-7 w-2/12" />
-                        <Skeleton className="h-5 w-1/12" />
+                        <Skeleton className="h-7 xs:w-4/12 lg:w-2/12" />
+                        <Skeleton className="h-5 xs:w-3/12 lg:w-1/12" />
                     </div>
-                    <div className="grid grid-cols-4 gap-2">
+                    <div className="grid gap-2 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         <LoadingCoursesCard numCards={4} />
                     </div>
                 </div>

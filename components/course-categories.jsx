2682a610d52f6ffb64217ac813d01f7c044ac5f2
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-18 23:57:09 +0800

    Update course-categories.jsx

--- components/course-categories.jsx
+++ components/course-categories.jsx
@@ -10,7 +10,7 @@ const CourseCategories = async () => {
             {categories.data.map((category, index) => (
                 <div
                     key={index}
-                    className="flex transform cursor-pointer items-center justify-center rounded-lg border bg-white px-2 text-xs text-black transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-50  dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900 xs:py-4 md:py-7 lg:w-full"
+                    className="flex h-16 w-full transform cursor-pointer items-center justify-center rounded-lg border bg-white px-2 text-xs text-black transition duration-300 ease-in-out hover:scale-110 hover:bg-gray-50 dark:bg-slate-950 dark:text-white dark:hover:bg-slate-900 xs:py-4 md:py-7 lg:w-full"
                 >
                     <span className="text-center">{category.name}</span>
                 </div>

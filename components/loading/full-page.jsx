6499f3de3af87f86b4f32b1f7cd11ae144db3b94
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-24 23:13:34 +0800

    Update full-page.jsx

--- components/loading/full-page.jsx
+++ components/loading/full-page.jsx
@@ -4,7 +4,7 @@ export default function LoadingFullPage() {
             <div role="status" className="-mt-28">
                 <svg
                     aria-hidden="true"
-                    class="h-12 w-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                    className="h-12 w-12 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ export default function LoadingFullPage() {
                         fill="currentFill"
                     />
                 </svg>
-                <span class="sr-only">Loading...</span>
+                <span className="sr-only">Loading...</span>
             </div>
         </div>
     );

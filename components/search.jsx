d86143deddb4daa8df0a499d9b599b0d88bc8979
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2023-11-30 15:56:12 +0800

    Update search.jsx

--- components/search.jsx
+++ components/search.jsx
@@ -40,12 +40,11 @@ export default function Search() {
                     ref={buttonRef}
                     className="ransition-colors relative inline-flex h-9 w-full items-center justify-start rounded-lg border  border-input bg-white/50 px-4 py-2 text-sm font-medium  text-gray-600 hover:bg-white hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 dark:bg-slate-900 dark:text-gray-100 xs:hidden sm:pr-12 md:inline-flex md:w-40 lg:w-64"
                 >
-                    <span className="hidden lg:inline-flex">
+                    <span className="line-clamp-1 flex-nowrap text-start">
                         {searchParams.has('q')
                             ? searchParams.get('q')
                             : 'Find Your Courses...'}
                     </span>
-                    <span className="inline-flex lg:hidden">Search...</span>
                     <kbd className="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
                         <span className="text-xs">⌘</span>K
                     </kbd>
